8a924a8da2e6b5fb1b16e16f439930b3dbcd19e8
Timothée Gauthier <t.gauthier42@gmail.com>
2019-03-29 22:25:47 +0100

    It really works !
    We must change door in order to increase the probability to win the car !

--- index.js
+++ index.js
@@ -14,19 +14,72 @@ function sum(a, ...b) {
 
 const freq = total => n => n * 100/total
 
+// 1) On affiche les 3 possibilites
+//
+// 2) le spectateur choisi une porte
+// 3) l'animateur montre une chevre
+//
+// 4) le spectateur fais un choix
+//
+// 4.1) "je change pas"
+// 4.2) "je change"
+
+
+// 1
+
+// // C chevre
+// // V voiture
+const possibilites = [
+    ['C', 'C', 'V'],
+    ['C', 'V', 'C'],
+    ['V', 'C', 'C'],
+]
+
 // expected output: 0, 1 or 2
 
-const a = [0, 0 ,0]
-for (let i = 0; i < 10000000; i++) {
-    const j = getRandomInt(3);
-    a[j]++;
-}
-console.log(a);
-const sumA = sum(...a);
-console.log('total :', sumA);
-console.log('freq :', a.map(freq(sumA)));
+function run() {
+    const portes = [...possibilites[getRandomInt(3)]];
 
-// // V voiture
-// A A V
-// A V A
-// V A A
+    // 2)
+    const choixSpec = getRandomInt(3);
+
+    let portesRestantes;
+
+    if (choixSpec === 0) {
+        portesRestantes = [portes[1], portes[2]]
+    } else if (choixSpec === 1) {
+        portesRestantes = [portes[0], portes[2]]
+    } else if (choixSpec === 2) {
+        portesRestantes = [portes[0], portes[1]]
+    }
+
+
+    // 3)
+    // on montre une chevre
+    const montreChevre = portesRestantes.findIndex(a => a === 'C')
+
+    let porteRestante;
+    if (montreChevre === 0) {
+        porteRestante = portesRestantes[1]
+    } else {
+        porteRestante = portesRestantes[0]
+    }
+
+    return portes[choixSpec];
+}
+
+
+let winIfKeep = 0;
+let winIfChange = 0;
+
+
+for (let i = 0; i < 10000000; i++) {
+    if (run() === 'V') {
+        winIfKeep++;
+    } else {
+        winIfChange++
+    }
+}
+
+
+console.log({winIfKeep, winIfChange});
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
 
 node index.js
+
+Output :
+
+{ winIfKeep: 3333098, winIfChange: 6666902 }
+
+
+// It means that people had more chance to win the Car
+if they change door
+
+I would say 1/3 to win if keep and 2/3 to win if change
+
