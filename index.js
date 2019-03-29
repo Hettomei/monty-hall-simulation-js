ef52f4064e471e01142a9e4cf99fc999c938900e
Timothée Gauthier <t.gauthier42@gmail.com>
2019-03-29 21:49:05 +0100

    check freqency

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+// console.log('monty hall problem');
+// console.log("d'abord afficher la frequence pour du rand simple");
 
-console.log('monty hall problem');
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function sum(a, ...b) {
+    if (a) {
+        return a + sum(...b);
+    }
+    return 0
+}
+
+const freq = total => n => n * 100/total
+
+// expected output: 0, 1 or 2
+
+const a = [0, 0 ,0]
+for (let i = 0; i < 10000000; i++) {
+    const j = getRandomInt(3);
+    a[j]++;
+}
+console.log(a);
+const sumA = sum(...a);
+console.log('total :', sumA);
+console.log('freq :', a.map(freq(sumA)));
+
+// // V voiture
+// A A V
+// A V A
+// V A A
